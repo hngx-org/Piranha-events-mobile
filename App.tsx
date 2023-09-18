611f76fe1465a5522a328d0938aa35ecedad298c
Olakunle Raheem <olaks.raheem@gmail.com>
2023-09-18 12:36:01 +0100

    Initial Commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  PaperProvider,
+  MD3LightTheme as DefaultTheme,
+} from "react-native-paper";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./screens/Home";
+
+//Please note that the authentication workflow will be handled by Dread.
+//See the react navigation authentication workflow for more information on setup
+//Theming will be handled by the group as a whole.
+
+const theme = {
+  // ...DefaultTheme,
+
+  colors: {
+    primary: "rgb(0, 104, 116)",
+    onPrimary: "rgb(255, 255, 255)",
+    primaryContainer: "rgb(151, 240, 255)",
+    onPrimaryContainer: "rgb(0, 31, 36)",
+    secondary: "rgb(0, 104, 116)",
+    onSecondary: "rgb(255, 255, 255)",
+    secondaryContainer: "rgb(151, 240, 255)",
+    onSecondaryContainer: "rgb(0, 31, 36)",
+    tertiary: "rgb(150, 73, 0)",
+    onTertiary: "rgb(255, 255, 255)",
+    tertiaryContainer: "rgb(255, 220, 198)",
+    onTertiaryContainer: "rgb(49, 19, 0)",
+    error: "rgb(186, 26, 26)",
+    onError: "rgb(255, 255, 255)",
+    errorContainer: "rgb(255, 218, 214)",
+    onErrorContainer: "rgb(65, 0, 2)",
+    background: "rgb(250, 253, 253)",
+    onBackground: "rgb(25, 28, 29)",
+    surface: "rgb(250, 253, 253)",
+    onSurface: "rgb(25, 28, 29)",
+    surfaceVariant: "rgb(219, 228, 230)",
+    onSurfaceVariant: "rgb(63, 72, 74)",
+    outline: "rgb(111, 121, 122)",
+    outlineVariant: "rgb(191, 200, 202)",
+    shadow: "rgb(0, 0, 0)",
+    scrim: "rgb(0, 0, 0)",
+    inverseSurface: "rgb(46, 49, 50)",
+    inverseOnSurface: "rgb(239, 241, 241)",
+    inversePrimary: "rgb(79, 216, 235)",
+    elevation: {
+      level0: "transparent",
+      level1: "rgb(238, 246, 246)",
+      level2: "rgb(230, 241, 242)",
+      level3: "rgb(223, 237, 238)",
+      level4: "rgb(220, 235, 237)",
+      level5: "rgb(215, 232, 234)",
+    },
+    surfaceDisabled: "rgba(25, 28, 29, 0.12)",
+    onSurfaceDisabled: "rgba(25, 28, 29, 0.38)",
+    backdrop: "rgba(41, 50, 52, 0.4)",
+  },
+};
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
